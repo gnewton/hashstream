5dbdbd17ee50072fdccbde62154d46c8f694d127
glen newton <glen.newton@gmail.com>
2015-08-15 11:23:07 -0500

    Updated docs

--- hashstream_reader.go
+++ hashstream_reader.go
@@ -1,6 +1,4 @@
 // Provides crypto hash Writer/Reader support for large files or streaming. <br> Inspired by Java's DigestInputStream/DigestOutputStream https://docs.oracle.com/javase/7/docs/api/java/security/DigestInputStream.html
-// Usage:
-//
 
 package hashstream
 
